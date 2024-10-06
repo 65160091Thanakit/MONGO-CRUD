4dd42d1525e6458ba04f408e4c5fcd914fb39edb
65160334 <65160334@go.buu.ac.th>
2024-10-06 14:46:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors'); // เพิ่มเพื่อจัดการ CORS
 
+
 // Import KPI routes
 const kpiRoutes = require('./routes/kpiRoutes');
+const itemRoutes = require('./routes/itemRoutes');
+
 
 // Create Express app
 const app = express();
@@ -23,6 +26,7 @@ mongoose.connect('mongodb://localhost:27017/crud-example', {
 
 // Use KPI routes
 app.use('/api', kpiRoutes); // ใช้เส้นทางสำหรับ KPI
+app.use('/api', itemRoutes); // ใช้เส้นทางสำหรับ KPI
 
 // Define a port
 const port = process.env.PORT || 3000;
